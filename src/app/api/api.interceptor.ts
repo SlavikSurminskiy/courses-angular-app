deafc92f4a9608419def87fb40943a2cd9445330
Vyacheslav <slaviksyrminskiy@gmail.com>
2021-04-12 04:30:57 +0300

    hw-8/add token to each api request

--- src/app/api/api.interceptor.ts
+++ src/app/api/api.interceptor.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+import { AuthService } from '../services/auth/auth.service';
 
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
   private BASE_URL = 'http://localhost:3004';
 
+  constructor(private _auth: AuthService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const request = req.clone({ url: `${this.BASE_URL}/${req.url}` });
+    const request = req.clone({
+      url: `${this.BASE_URL}/${req.url}`,
+      headers: new HttpHeaders({
+        Authorization: this._auth.getToken(),
+      }),
+    });
 
     return next.handle(request);
   }
